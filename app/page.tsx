b584004e7cd0b07388df9424905b23acca3cf95e
easyblend <kenzieemma072@gmail.com>
2025-12-08 23:55:31 +0100

    style: added light/dark team with proper landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,64 @@
+"use client";
 import Image from "next/image";
+import Link from "next/link";
+import ThemeToggle from "./component/ThemeToggle";
 
-export default function Home() {
+export default function Page() {
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-50 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="flex flex-col items-center justify-center text-center px-4 bg-[url('/assets/light-hero-gradient.svg')] dark:bg-[url('/assets/dark-hero-gradient.svg')] bg-no-repeat bg-cover">
+
+      {/* Community Badge */}
+      <div className="flex flex-wrap items-center justify-center gap-3 p-1.5 pr-4 mt-46 rounded-full border border-slate-300 dark:border-slate-600 bg-white/70 dark:bg-slate-600/20">
+        <div className="flex items-center -space-x-3">
+          <Image
+            className="size-7 rounded-full"
+            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=50&auto=format"
+            height={50} width={50} alt="User 1"
+          />
+          <Image
+            className="size-7 rounded-full"
+            src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=50&auto=format"
+            height={50} width={50} alt="User 2"
+          />
+          <Image
+            className="size-7 rounded-full"
+            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=50&h=50&auto=format&fit=crop"
+            height={50} width={50} alt="User 3"
+          />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+
+        <p className="text-xs">Join testers & founders improving products daily</p>
+      </div>
+
+      {/* Main Hero Text */}
+      <h1 className="mt-2 text-5xl/15 md:text-[64px]/19 font-semibold max-w-3xl">
+        Launch better products with{" "}
+        <span className="bg-linear-to-r from-[#923FEF] dark:from-[#C99DFF] to-[#C35DE8] dark:to-[#E1C9FF] bg-clip-text text-transparent">
+          real testers
+        </span>
+      </h1>
+
+      <p className="text-base dark:text-slate-300 max-w-xl mt-3">
+        CrowdTest helps you catch bugs early with real pre-launch testers.
+        Join the waitlist to be part of the first beta users.
+      </p>
+
+      {/* CTA Buttons */}
+      <div className="flex items-center gap-4 mt-8">
+        <Link href="/waitlist">
+          <button className="bg-purple-600 hover:bg-purple-700 transition text-white rounded-md px-6 h-11">
+            Join Waitlist
+          </button>
+        </Link>
+      </div>
+
+      {/* Subheader */}
+      <h3 className="text-base text-center text-slate-400 mt-28 pb-14 font-medium">
+        Backed by a community of passionate testers & founders
+      </h3>
+
+      <ThemeToggle />
     </div>
   );
 }
