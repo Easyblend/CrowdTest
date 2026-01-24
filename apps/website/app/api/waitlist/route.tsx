9c271194d4b36a1b26bd2f2ca1fe99d2e8a4c32b
easyblend <kenzieemma072@gmail.com>
2026-01-24 14:39:35 +0100

    refactor: use staging url in staging environment

--- apps/website/app/api/waitlist/route.tsx
+++ apps/website/app/api/waitlist/route.tsx
@@ -36,7 +36,7 @@ export async function POST(req: NextRequest) {
     },
   });
 
-  const confirmUrl = `${process.env.NEXT_PUBLIC_SITE_URL}/api/waitlist/confirm?token=${token}`;
+  const confirmUrl = `${process.env.NEXT_PUBLIC_SITE_URL_STG}/api/waitlist/confirm?token=${token}`;
 
   await transporter.sendMail({
     from: '"CrowdTest" <hello@crowdtest.dev>',
